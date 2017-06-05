3057303b3bbc1af52f67a8c727bfc1a60eee3827
qc1iu <qc1iu@vip.qq.com>
2017-06-05 09:25:22 +0800

    for my emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,11 @@
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+(require 'auto-complete-config)
+(ac-config-default)
+
