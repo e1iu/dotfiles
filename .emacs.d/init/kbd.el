3e393478e540757a91102744fd01a2e4a11dd300
qc1iu <qc1iu@vip.qq.com>
2017-08-02 09:11:00 +0800

    new

--- .emacs.d/init/kbd.el
+++
@@ -1,5 +0,0 @@
-(global-set-key [f12] 'sr-speedbar-toggle)
-(global-set-key [f8] 'neotree-toggle)
-(global-set-key [f9] 'color-theme-gnome2)
-(define-key global-map [C-return] 'set-mark-command)
-(global-set-key "%" 'match-paren)
