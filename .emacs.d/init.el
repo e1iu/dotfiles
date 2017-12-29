789497b828912491249e992560edc17e9b199211
qc1iu <qc1iu@vip.qq.com>
2017-12-29 13:35:35 +0800

    mac Command remap for emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -116,3 +116,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(when (eq system-type 'darwin)
+    (setq mac-command-modifier 'meta)
+      (setq mac-option-modifier nil))
