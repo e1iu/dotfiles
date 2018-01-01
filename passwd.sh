09b262fcb7e2b7d2a0a08a79834279235756a7bd
qc1iu <qc1iu@vip.qq.com>
2018-01-01 17:24:30 +0800

    add passwd.sh

---
+++ passwd.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+prefix="/Users/qc1iu/"
+in=".pwd.txt"
+out=".output.txt"
+key=${1}
+
+if [ ! -f ${prefix}${out} ]; then
+  gpg2 --output ${prefix}${out} --decrypt ${prefix}${in}
+fi
+
+if [ 1 -eq $? ]; then
+  exit
+fi
+
+
+if [ "$key" == "" ]; then
+  cat ${prefix}${out} | while read LINE;  
+do
+  echo ${LINE} | awk '{print $1}'
+done
+exit
+fi
+
+if [ -f ${prefix}${out} ]; then
+  cat ${prefix}${out} | while read LINE;  
+do
+  c=`echo ${LINE} | awk '{print $1}'`
+  if [ "$key" == "$c" ]; then
+    echo ${LINE} | awk '{print $2 "\t" $3}'
+    break
+  fi
+done
+fi 
