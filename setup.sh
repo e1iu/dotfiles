dac8f470da4fe53e0c43da0a5c3a21dd4e32a30b
eric <qc1iuone@outlook.com>
2019-08-28 13:26:21 +0000

    Update setup.sh
    This patch refined the setup.sh
    
    1. Support mac by using GNU utils.
    2. Use 'ln -b' to backup if LINK_NAME already exist.
    3. Install vim plugin and fzf.
    
    Issue: #3

--- setup.sh
+++ setup.sh
@@ -1,35 +1,68 @@
 #! /bin/bash
+set -e
+
+msg () {
+  echo "MSG: $@" >&2
+}
+
+err () {
+  echo "ERR: $@" >&2
+}
+
+banner () {
+  local banner_line="## [$@] ##"
+  msg "$(printf '%0.s#' $(seq 1 ${#banner_line}))"
+  msg $banner_line
+  msg "$(printf '%0.s#' $(seq 1 ${#banner_line}))"
+}
+
+gnu_ln=""
+case "$(uname)" in
+  Darwin*)
+    gnu_ln="gln"
+    ;;
+  Linux*)
+    gnu_ln="ln"
+    ;;
+  *)
+    err "Only support Darwin or Linux."
+    exit 1
+esac
+
+# Ensure the GNU link has been installed.
+# See https://apple.stackexchange.com/questions/69223/how-to-replace-mac-os-x-utilities-with-gnu-core-utilities
+$gnu_ln --version > /dev/null
 
 DOT_TOP=$(realpath $(dirname $BASH_SOURCE))
 
-echo "### Setup bash configuration ###"
+banner "Setup bash configuration"
 
-echo "  Create soft link to .bashrc..."
-[ -s ~/.bashrc ] && [ ! -L ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
-ln -sf -T $DOT_TOP/bashrc ~/.bashrc
+msg "Create soft link to .bashrc"
+$gnu_ln -sfb -T $DOT_TOP/bash_profile ~/.bash_profile
 
-echo "  Create soft link to .bash_aliases..."
-[ -s ~/.bash_aliases ] && [ ! -L ~/.bash_aliases ] && \
-  mv ~/.bash_aliases ~/.bash_aliases.bak
-ln -sf -T $DOT_TOP/bash_aliases ~/.bash_aliases
+msg "Create soft link to .bashrc"
+$gnu_ln -sfb -T $DOT_TOP/bashrc ~/.bashrc
 
-echo "  Create soft link to .bash_completions..."
-[ -s ~/.bash_completions ] && [ ! -L ~/.bash_completions ] && \
-  mv ~/.bash_completions ~/.bash_completions.bak
-ln -sf -T $DOT_TOP/bash_completions ~/.bash_completions
+msg "Create soft link to .bash_aliases"
+$gnu_ln -sfb -T $DOT_TOP/bash_aliases ~/.bash_aliases
 
+msg "Create soft link to .bash_completions"
+$gnu_ln -sfb -T $DOT_TOP/bash_completions ~/.bash_completions
 
 echo ""
-echo "### Setup vim configuration ###"
+banner "Setup vim configuration"
 
-echo "  Create soft link to .vimrc..."
-[ -s ~/.vimrc ] && [ ! -L ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
-ln -sf -T $DOT_TOP/vimrc ~/.vimrc
+msg "Create soft link to .vimrc"
+$gnu_ln -sfb -T $DOT_TOP/vimrc ~/.vimrc
 
-echo "  Create soft link to .vim..."
-[ -s ~/.vim ] && [ ! -L ~/.vim ] && mv ~/.vim ~/.vim.bak
-ln -sf -T $DOT_TOP/vim ~/.vim
+msg "Create soft link to .vim"
+$gnu_ln -sfb -T $DOT_TOP/vim ~/.vim
 
-echo "  Install Vundle from github..."
-[ ! -s ~/.vim/bundle/Vundle.vim ] && \
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+msg "Install Vundle from GitHub"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+msg "Install Plugin"
+vim +PluginInstall +qall
+
+msg "Install fzf"
+bash $DOT_TOP/vim/bundle/fzf/install
